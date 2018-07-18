19549c8e2fb424d80d701f4d048a9c0d262d0824
zhekbland <eshpytev@mail.ru>
2018-07-18 19:39:45 +0300

    6.1. Classic brute force search.

---
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * @author Evgeny Shpytev (mailto:eshpytev@mail.ru)
+ * @version $Id$
+ * @since  0.1
+ */
+public class FindLoop {
+    /**
+     * Find the number in the array.
+     * @param data array.
+     * @param el find number.
+     * @return el.
+     */
+    public int indexOf(int[] data, int el) {
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,44 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Evgeny Shpytev (mailto:eshpytev@mail.ru)
+ * @version $Id$
+ * @since  0.1
+ */
+public class FindLoopTest {
+
+    @Test
+    public void whenArrayHasLengh5Then0() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 5;
+        int result = find.indexOf(input, value);
+        int expect = 0;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasLengh3Then2() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 3;
+        int result = find.indexOf(input, value);
+        int expect = 2;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayHasLenghNoThen1() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {5, 10, 3};
+        int value = 15;
+        int result = find.indexOf(input, value);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+}
