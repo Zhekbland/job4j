988a5b8ef172e661b2c4dbc20249be709b51135b
zhekbland <eshpytev@mail.ru>
2018-07-19 14:31:48 +0300

    6.2. Turn the array.

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * @author Evgeny Shpytev (mailto:eshpytev@mail.ru)
+ * @version $Id$
+ * @since  0.1
+ */
+public class Turn {
+
+    /**
+     * Turn the array.
+     * @param array right array.
+     * @return turn array.
+     */
+    public int[] turn(int[] array) {
+        for (int index = 0; index < array.length / 2; index++) {
+            int temp = array[index];
+            array[index] = array[array.length - 1 - index];
+            array[array.length - 1 - index] = temp;
+        }
+        return array;
+    }
+
+}
