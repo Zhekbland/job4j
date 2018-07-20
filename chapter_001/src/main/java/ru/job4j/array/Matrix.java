9924aabce1380035ed6df951f8c43886c6020a0e
zhekbland <eshpytev@mail.ru>
2018-07-20 19:44:37 +0300

    6.6. Two-dimensional array, table of multiplication.

---
+++ chapter_001/src/main/java/ru/job4j/array/Matrix.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+/**
+ * @author Evgeny Shpytev (mailto:eshpytev@mail.ru)
+ * @version $Id$
+ * @since  0.1
+ */
+public class Matrix {
+
+    /**
+     * Create table of multiplication.
+     * @param size array matrix.
+     * @return table of multiplication.
+     */
+    public int[][] multiple(int size) {
+        int[][] table = new int[size][size];
+        for (int i = 0; i < table.length; i++) {
+            for (int j = 0; j < table.length; j++) {
+                table[i][j] = (i + 1) * (j + 1);
+            }
+        }
+        return table;
+    }
+}
