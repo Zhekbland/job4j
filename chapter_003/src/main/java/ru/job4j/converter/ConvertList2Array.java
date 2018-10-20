2ca32862689cfa91b95c76ce73704da4a3a50b47
zhekbland <eshpytev@mail.ru>
2018-10-20 16:11:14 +0300

    Collections 2.1. Convert List of Arrays to List.

--- chapter_003/src/main/java/ru/job4j/converter/ConvertList2Array.java
+++ chapter_003/src/main/java/ru/job4j/converter/ConvertList2Array.java
@@ -1,5 +1,6 @@
 package ru.job4j.converter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ConvertList2Array {
@@ -17,5 +18,15 @@ public class ConvertList2Array {
             }
         }
         return array;
+    }
+
+    public List<Integer> convert(List<int[]> list) {
+        List<Integer> listOfIntElements = new ArrayList<>();
+        for (int[] arraysInList : list) {
+            for (int elementsInArray : arraysInList) {
+                listOfIntElements.add(elementsInArray);
+            }
+        }
+        return listOfIntElements;
     }
 }
--- chapter_003/src/test/java/ru/job4j/converter/ConverterList2ArrayTest.java
+++ chapter_003/src/test/java/ru/job4j/converter/ConverterList2ArrayTest.java
@@ -2,7 +2,9 @@ package ru.job4j.converter;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -35,6 +37,17 @@ public class ConverterList2ArrayTest {
                 {3, 4},
                 {5, 0}
         };
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenListOfArraysConvertToListOfIntegers() {
+        ConvertList2Array convertList = new ConvertList2Array();
+        List<int[]> listOfArrays = new ArrayList<>();
+        listOfArrays.add(new int[]{1, 2});
+        listOfArrays.add(new int[]{3, 4, 5});
+        List<Integer> result = convertList.convert(listOfArrays);
+        List<Integer> expect = Arrays.asList(1, 2, 3, 4, 5);
         assertThat(result, is(expect));
     }
 }
