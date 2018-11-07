c295cc4c4b5bb7a37ca4eaf3e7291afaf62c67e3
zhekbland <eshpytev@mail.ru>
2018-11-07 14:45:55 +0300

    Lambda 0. Functional interfaces.

---
+++ chapter_004/src/main/java/ru/job4j/lambda/Calculator.java
@@ -0,0 +1,18 @@
+package ru.job4j.lambda;
+
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+
+public class Calculator {
+    public interface Operation {
+        double calc(int left, int right);
+    }
+
+    public void multiple(int start, int finish, int value,
+                         BiFunction<Integer, Integer, Double> op,
+                         Consumer<Double> media) {
+        for (int index = start; index != finish; index++) {
+            media.accept(op.apply(value, index));
+        }
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/lambda/CalculatorTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void wnenAdd1Unitl3() {
+        Calculator calc = new Calculator();
+        List<Double> buffer = new ArrayList<>();
+        calc.multiple(
+                0, 3, 1,
+                (value, index) -> (double) value + index,
+                result -> buffer.add(result)
+        );
+        assertThat(buffer, is(Arrays.asList(1D, 2D, 3D)));
+    }
+}
