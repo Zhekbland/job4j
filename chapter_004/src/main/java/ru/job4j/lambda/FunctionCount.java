4c69d0ca5bfceadbc307be36bd94a5e006062450
zhekbland <eshpytev@mail.ru>
2018-11-08 13:13:00 +0300

    Lambda 1. Counting function in range(update v.2).

--- chapter_004/src/main/java/ru/job4j/lambda/FunctionCount.java
+++ chapter_004/src/main/java/ru/job4j/lambda/FunctionCount.java
@@ -8,22 +8,10 @@ import java.util.function.Function;
  * Class FunctionCount counts lineFunction, squareFunction and logarithmicFunction.
  *
  * @author Evgeny Shpytev (mailto:eshpytev@mail.ru).
- * @version 1.
- * @since 07.11.2018.
+ * @version 2.
+ * @since 08.11.2018.
  */
 public class FunctionCount {
-    public double lineFunction(double x) {
-        return 3 + x;
-    }
-
-    public double squareFunction(double x) {
-        return Math.pow(x, 2);
-    }
-
-    public double logarithmicFunction(double x) {
-        return Math.log(x);
-    }
-
     public List<Double> diapason(int start, int end, Function<Double, Double> func) {
         List<Double> functions = new ArrayList<>();
         for (int index = start; index <= end; index++) {
--- chapter_004/src/test/java/ru/job4j/lambda/FunctionCountTest.java
+++ chapter_004/src/test/java/ru/job4j/lambda/FunctionCountTest.java
@@ -12,8 +12,8 @@ import static org.junit.Assert.assertThat;
  * Class FunctionCountTest tests FunctionCount.
  *
  * @author Evgeny Shpytev (mailto:eshpytev@mail.ru).
- * @version 1.
- * @since 07.11.2018.
+ * @version 2.
+ * @since 08.11.2018.
  */
 public class FunctionCountTest {
     @Test
@@ -21,11 +21,11 @@ public class FunctionCountTest {
         FunctionCount function = new FunctionCount();
         List<Double> result = function.diapason(
                 0, 5,
-                function::lineFunction
+                index -> (double) 3 + index
         );
         List<Double> except = new ArrayList<>();
         for (int index = 0; index <= 5; index++) {
-            except.add(function.lineFunction(index));
+            except.add((double) 3 + index);
         }
         assertThat(result, is(except));
     }
@@ -34,12 +34,12 @@ public class FunctionCountTest {
     public void squareFunctionTest() {
         FunctionCount function = new FunctionCount();
         List<Double> result = function.diapason(
-                0, 6,
-                function::squareFunction
+                1, 6,
+                index -> Math.pow(index, 2)
         );
         List<Double> except = new ArrayList<>();
-        for (int index = 0; index <= 6; index++) {
-            except.add(function.squareFunction(index));
+        for (int index = 1; index <= 6; index++) {
+            except.add(Math.pow(index, 2));
         }
         assertThat(result, is(except));
     }
@@ -48,12 +48,12 @@ public class FunctionCountTest {
     public void logarithmicTest() {
         FunctionCount function = new FunctionCount();
         List<Double> result = function.diapason(
-                0, 4,
-                function::logarithmicFunction
+                1, 7,
+                index -> (Math.log(index))
         );
         List<Double> except = new ArrayList<>();
-        for (int index = 0; index <= 4; index++) {
-            except.add(function.logarithmicFunction(index));
+        for (int index = 1; index <= 7; index++) {
+            except.add(Math.log(index));
         }
         assertThat(result, is(except));
     }
