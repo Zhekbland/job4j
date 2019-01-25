a0100ce869a37a9bccf87005ddf11c5d78942454
zhekbland <eshpytev@mail.ru>
2019-01-25 17:49:30 +0300

    Multithreading. Wait and Notify. 2.0 Parallel search.

---
+++ chapter_006/src/test/java/ru/job4j/wait/ParallelSearchTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.wait;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ParallelSearchTest {
+    @Test
+    public void whenWeDoParallelSearch() {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        queue.offer(index);
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    consumer.interrupt();
+                }
+
+        ).start();
+    }
+}
