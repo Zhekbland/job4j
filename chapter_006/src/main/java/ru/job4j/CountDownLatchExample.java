05d3788f155b72ee53cfc0b80408b6b9706f7a0b
zhekbland <eshpytev@mail.ru>
2019-03-31 16:33:31 +0300

    Threads. Examples Semaphore, CountDownLatch, ReentrantLock.

---
+++ chapter_006/src/main/java/ru/job4j/CountDownLatchExample.java
@@ -0,0 +1,49 @@
+package ru.job4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+// CountDownLatch
+public class CountDownLatchExample {
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
+
+        for (int i = 0; i < 3; i++) {
+            executorService.submit(new Processor(i, countDownLatch));
+        }
+
+        executorService.shutdown();
+
+        for (int i = 0; i < 3; i++) {
+            Thread.sleep(1000);
+            countDownLatch.countDown();
+        }
+    }
+}
+
+class Processor implements Runnable {
+    private CountDownLatch countDownLatch;
+    private int id;
+
+    public Processor(int id, CountDownLatch countDownLatch) {
+        this.id = id;
+        this.countDownLatch = countDownLatch;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            this.countDownLatch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Thread with id" + id + " proceeded");
+    }
+}
---
+++ chapter_006/src/main/java/ru/job4j/SemaphoreExample.java
@@ -0,0 +1,60 @@
+package ru.job4j;
+
+import java.util.concurrent.*;
+
+//Semaphore
+public class SemaphoreExample {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(200);
+        Connection connection = Connection.getConnection();
+        for (int i = 0; i < 200; i++) {
+            executorService.submit(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        connection.work();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.DAYS);
+    }
+}
+
+//Singleton
+class Connection {
+    private static Connection connection = new Connection();
+    private int connectionsCount;
+    private Semaphore semaphore = new Semaphore(10);
+
+    private Connection() {
+
+    }
+
+    public static Connection getConnection() {
+        return connection;
+    }
+
+    public void work() throws InterruptedException {
+        semaphore.acquire();
+        try {
+            doWork(); //Чтобы не произошло с doWork, finally выполнтиься в любом случае.
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private void doWork() throws InterruptedException {
+        synchronized (this) {
+            connectionsCount++;
+            System.out.println(connectionsCount);
+        }
+        Thread.sleep(5000);
+        synchronized (this) {
+            connectionsCount--;
+        }
+    }
+}
