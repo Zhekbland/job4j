e9e9fe87519ca6d5cf9d850b5b8ca74d6214a9c6
zhekbland <eshpytev@mail.ru>
2019-04-12 14:04:26 +0300

    OOP. The distance between points in three-dimensional. [#122643]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,11 +1,9 @@
 package ru.job4j.condition;
 
-import com.sun.javafx.binding.StringFormatter;
-
 /**
  * Class Point finds points of x and y.
  * @author Evgeny Shpytev (mailto:eshpytev@mail.ru)
- * @version 3
+ * @version 4
  * @since 08.04.2019
  */
 public class Point {
@@ -21,6 +19,11 @@ public class Point {
     private int y;
 
     /**
+     * Contains z coordinate.
+     */
+    private int z;
+
+    /**
      * Constructor takes variables.
      *
      * @param first  - x.
@@ -29,6 +32,19 @@ public class Point {
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    /**
+     * Constructor takes variables.
+     *
+     * @param first  - x.
+     * @param second - y.
+     * @param third  - z.
+     */
+    public Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
     }
 
     /**
@@ -42,9 +58,27 @@ public class Point {
     }
 
     /**
+     * The distance between points in the coordinate system.
+     *
+     * @param that - another Point with coordinates x, y, z.
+     * @return distance.
+     */
+    public double distanceThree(Point that) {
+        return Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
+                + Math.pow(this.z - that.z, 2));
+    }
+
+    /**
      * Print.
      */
     public void info() {
         System.out.println(String.format("Point[%s, %s]", this.x, this.y));
+    }
+
+    /**
+     * Print.
+     */
+    public void infoThreeDimensional() {
+        System.out.println(String.format("Point[%s, %s, %s]", this.x, this.y, this.z));
     }
 }
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -2,6 +2,7 @@ package ru.job4j.condition;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.closeTo;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
@@ -9,7 +10,7 @@ import static org.junit.Assert.*;
  * Class Point finds points of x and y.
  *
  * @author Evgeny Shpytev (mailto:eshpytev@mail.ru)
- * @version 2
+ * @version 3
  * @since 08.04.2019
  */
 public class PointTest {
@@ -38,5 +39,31 @@ public class PointTest {
         first.info();
         Point second = new Point(2, 2);
         second.info();
+    }
+
+    @Test
+    public void whenZeroAndTenThenTenThreeDimensional() {
+        Point first = new Point(0, 8, 4);
+        Point second = new Point(16, 10, 20);
+        double result = first.distanceThree(second);
+        first.infoThreeDimensional();
+        second.infoThreeDimensional();
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, closeTo(23D, 0.3));
+    }
+
+    @Test
+    public void whenCheckItselfThreeDimensional() {
+        Point point = new Point(0, 0, 0);
+        double result = point.distanceThree(point);
+        assertThat(result, is(0D));
+    }
+
+    @Test
+    public void whenShowInfoThreeDimensional() {
+        Point first = new Point(1, 1, 2);
+        first.infoThreeDimensional();
+        Point second = new Point(5, 5, 5);
+        second.infoThreeDimensional();
     }
 }
